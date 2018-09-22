bc7b280743857023010aa27709f4f2332cd7bf58
barbosafernandes <warlley_1996@hotmail.com>
2018-09-21 22:50:39 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title></title>
+</head>
+<body>
+	 <div id="logo"></div>
+	<nav id="menu">
+		<ul id="menu2">	
+			<li><a href="">Login</a></li>
+			<li><a href="">Cadastro</a></li>
+		</ul>
+	</nav>
+	<main id="corpo">
+		<div class="a">
+			<!--<video autoplay="true" loop controls muted style="width: 100%; height: 100%;">
+				<source src="https://youtu.be/ogL5smZ0tIw" type="video/mp4" >
+			</video>
+		-->
+
+		<iframe width="100%" height="100%" src="https://www.youtube.com/embed/or3U2rXxvQw" frameborder="9" allow="autoplay; encrypted-media" allowfullscreen>
+			
+		</iframe>
+		</div>
+		<div class="b">Lugar de lixo é no lugar correto!
+A Política Nacional de Resíduos Sólidos está em vigor desde agosto de 2010 e os planos municipais de gestão integrada desses materiais foram (ou deveriam ter sido) apresentados até agosto de 2012. Desde então, o poder público e a sociedade dividem a responsabilidade pelo destino dado ao lixo produzido. Conhecer o que é reciclável e como você deve separar e descartar os rejeitos da sua casa são pontos importantes para cumprir essa imprescindível tarefa! E reforçamos que a saúde do meio ambiente está diretamente relacionada à saúde de todos os seres vivos, incluindo todos nós! Confira este Papo Saúde repleto de informações sobre a reciclagem! (e não esqueça de compartilhar!).</div>
+		
+		<div class="a"></div>
+		<div class="a"></div>
+		<div class="a"></div>
+		<div class="b">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+		<div class="a"></div>
+		<div class="b">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+
+	</main>
+	
+	<style>
+		.a:nth-child(2){
+			order: ;
+		}
+		
+	</style>
+
+</body>
+</html>
