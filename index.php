f98c9ac8931da9416f356e708b2bfa733d08a3a1
barbosafernandes <warlley_1996@hotmail.com>
2018-09-22 16:26:15 -0300

    edição deigner

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
 	<link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,8 +13,11 @@
 			<li><a href="">Anuncie</a></li>
 		</ul>
 	</nav>
+
+
 	<main id="corpo">
-		<div id="bloco01">
+
+	<div id="bloco01">
 			<!--<video autoplay="true" loop controls muted style="width: 100%; height: 100%;">
 				<source src="https://youtu.be/ogL5smZ0tIw" type="video/mp4" >
 			</video>
@@ -23,12 +26,13 @@
 		<iframe width="100%" height="100%" src="https://www.youtube.com/embed/ZO-jQjEdK9w" frameborder="9" allow="autoplay; encrypted-media" allowfullscreen>
 			
 		</iframe>
+
 		</div>
 		<div id="blocotxt01">
 		<p>Lugar de lixo é no lugar correto!
-			A Política Nacional de Resíduos Sólidos está em vigor desde agosto de 2010 e os planos municipais de gestão integrada desses materiais foram (ou deveriam ter sido) apresentados até agosto de 2012. Desde então, o poder público e a sociedade dividem a responsabilidade pelo destino dado ao lixo produzido. Conhecer o que é reciclável e como você deve separar e descartar os rejeitos da sua casa são pontos importantes para cumprir essa imprescindível tarefa! E reforçamos que a saúde do meio ambiente está diretamente relacionada à saúde de todos os seres vivos, incluindo todos nós! Confira este Papo Saúde repleto de informações sobre a reciclagem! (e não esqueça de compartilhar!)
+			A Política Nacional de Resíduos Sólidos está em vigor desde agosto de 2010 e os planos municipais de gestão integrada desses materiais foram (ou deveriam ter sido) apresentados até agosto de 2012. Desde então, o poder público e a sociedade dividem a responsabilidade pelo destino dado ao lixo produzido.
 		</p>
-		.</div>
+		</div>
 		
 		<a href="">
 		<div id="bloco02">
